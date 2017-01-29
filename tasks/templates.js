52a50191ece89563387fb597cec4f1bf7bd688ec
Artem Sapegin <artem@sapegin.ru>
2017-01-29 18:28:44 +0100

    feat: GitHub templates

---
+++ tasks/templates.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const cpFile = require('cp-file');
+const chalk = require('chalk');
+
+const files = [
+  '.github/ISSUE_TEMPLATE.md',
+  '.github/PULL_REQUEST_TEMPLATE.md',
+];
+
+module.exports = () => {
+  files.forEach((file) => {
+    console.log(chalk.green(`Copy ${file}`)); // eslint-disable-line no-console
+    cpFile(path.resolve(__dirname, '..', file), file);
+  });
+};
