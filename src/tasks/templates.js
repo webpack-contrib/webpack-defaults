d2ce45d129c1e8b87640756f567a02936bd1c35e
Artem Sapegin <artem@sapegin.ru>
2017-02-02 18:57:27 +0100

    feat: add license and change log

--- src/tasks/templates.js
+++ src/tasks/templates.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const { copyFiles } = require('mrm-core');
 
+// These files will be overwritten without any confirmation
 const files = [
+  'LICENSE',
+  'CHANGELOG.md',
   '.github/ISSUE_TEMPLATE.md',
   '.github/PULL_REQUEST_TEMPLATE.md',
 ];
 
 module.exports = () => {
-  copyFiles(path.resolve(__dirname, '../..'), files);
+  copyFiles(path.resolve(__dirname, '../../templates'), files);
 };
