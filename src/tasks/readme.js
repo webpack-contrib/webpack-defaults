0bcffe01c3fd0985d5eb8a9d0f6580b3ba044b64
Artem Sapegin <artem@sapegin.ru>
2017-02-02 21:46:19 +0100

    feat: add readme

---
+++ src/tasks/readme.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const { json, template } = require('mrm-core');
+
+module.exports = () => {
+  const pkg = json('package.json');
+
+  // Create Readme.md (no update)
+  const readme = template('README', path.join(__dirname, '../../templates/README.template'));
+  if (!readme.exists()) {
+    readme
+      .apply({
+        package: pkg.get('name'),
+        description: pkg.get('description') || 'DESCRIPTION',
+      })
+      .save();
+  }
+};
