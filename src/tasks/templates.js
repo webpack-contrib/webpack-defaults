841b9c457ae6a8d37016f4e07cc9864a9b023294
Joshua Wiens <wiens.joshua@gmail.com>
2017-06-10 03:47:57 -0500

    feat: Implement Travis stages (#69)
    - Runs canary stage as an allowed failure
     - Moves travis.yml to a template until the anchor / alias issue can be resolved
     - Comment related to the above added to travis.js

--- src/tasks/templates.js
+++ src/tasks/templates.js
@@ -3,6 +3,7 @@ const { copyFiles } = require('mrm-core');
 
 // These files will be overwritten without any confirmation
 const files = [
+  '.travis.yml',
   'LICENSE',
   '.github/ISSUE_TEMPLATE.md',
   '.github/PULL_REQUEST_TEMPLATE.md',
