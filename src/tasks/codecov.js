b87c9c9b53ec30bd19f10b83555e034b0526940e
Steven Hargrove <steven.hargrove@gmail.com>
2017-06-27 12:13:42 -0400

    added codecov script

---
+++ src/tasks/codecov.js
@@ -0,0 +1,37 @@
+const { yaml } = require('mrm-core');
+
+module.exports = () => {
+  // codecov.yml
+  yaml('codecov.yml')
+    // Overwrite
+    .set({
+      codecov: {
+        branch: 'master',
+      },
+      coverage: {
+        precision: 2,
+        round: 'down',
+        range: '70...100',
+        status: {
+          project: {
+            default: {
+              target: 'auto',
+            },
+          },
+          patch: {
+            default: {
+              target: 'auto',
+            },
+          },
+          changes: {
+            default: {
+              branches: null,
+            },
+          },
+        },
+      },
+      comment: 'off',
+    })
+    .save()
+  ;
+};
