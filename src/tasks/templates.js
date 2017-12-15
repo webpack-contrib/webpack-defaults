8a841ac474d8b4503d1dc7aebf864bec0e2b3904
Joshua Wiens <joshuaw@easymetrics.com>
2017-12-15 05:15:11 -0600

    feat: Adds codeowners template

--- src/tasks/templates.js
+++ src/tasks/templates.js
@@ -7,6 +7,7 @@ const files = [
   '.circleci/config.yml',
   'commitlint.config.js',
   'LICENSE',
+  '.github/CODEOWNERS',
   '.github/ISSUE_TEMPLATE.md',
   '.github/PULL_REQUEST_TEMPLATE.md',
   '.prettierrc',
