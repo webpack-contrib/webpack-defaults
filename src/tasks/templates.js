9fcf155382c2599f767c6f2c5c277b77f2b916ed
shellscape <andrew@shellscape.org>
2018-03-27 16:59:33 -0400

    feat(dotfile): add .editorconfig to templates

--- src/tasks/templates.js
+++ src/tasks/templates.js
@@ -5,15 +5,16 @@ const { copyFiles } = require('mrm-core');
 // These files will be overwritten without any confirmation
 const files = [
   '.circleci/config.yml',
-  '.vscode/launch.json',
-  'commitlint.config.js',
-  'LICENSE',
   '.github/CODEOWNERS',
   '.github/ISSUE_TEMPLATE.md',
   '.github/PULL_REQUEST_TEMPLATE.md',
-  '.prettierrc',
-  'appveyor.yml',
+  '.vscode/launch.json',
+  '.editorconfig',
   '.eslintrc.js',
+  '.prettierrc',
+  'commitlint.config.js',
+  'appveyor.yml',
+  'LICENSE',
 ];
 
 const testFiles = [
