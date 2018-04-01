c5bed3c92b078c36dd1e715200466aae89d7783c
Andrew Powell <shellscape@users.noreply.github.com>
2018-04-01 04:33:18 -0700

    fix: add keywords to package, replace name in issue template (#105)

---
+++ src/tasks/issue_template.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const { json, template } = require('mrm-core');
+
+module.exports = () => {
+  const pkg = json('package.json');
+
+  // Create README.md (no update)
+  const file = template(
+    '.github/ISSUE_TEMPLATE.md',
+    path.join(__dirname, '../../templates/.gitub/ISSUE_TEMPLATE.md')
+  );
+
+  if (!file.exists()) {
+    file.apply({ package: pkg.get('name') }).save();
+  }
+};
