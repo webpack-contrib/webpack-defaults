85f6c73ca8c0b3ee0f5a70fbd9576e3d789d7016
evilebottnawi <sheo13666q@gmail.com>
2019-11-22 20:44:26 +0300

    refactor: test name

--- templates/test/validate-options.test.js
+++ templates/test/validate-options.test.js
@@ -1,6 +1,6 @@
 import { getCompiler, compile } from './helpers';
 
-describe('validation', () => {
+describe('validate options', () => {
   const tests = {
     name: {
       success: [1, true, false, 'test', /test/, [], {}, { foo: 'bar' }],
